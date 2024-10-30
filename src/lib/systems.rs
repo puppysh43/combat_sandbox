9aa70d181370a179793346613e66469030b6b332
puppysh43 <throwawayforher1997@gmail.com>
2024-10-30 15:47:33 -0400

    added dice rolls

--- src/lib/systems.rs
+++ src/lib/systems.rs
@@ -3,6 +3,7 @@ This module contains all the helper functions for the game systems
 */
 use crate::prelude::*;
 use hecs::*;
+use macroquad::rand;
 
 pub fn calculate_initiative(state: &mut GameState) {
     //in the future this will take in arguments either what entities will be included or what mapscreen will have the combat encounter in it or WHATEVER
@@ -19,5 +20,74 @@ pub fn refresh_mp(state: &mut GameState, active_entity: Entity) {
     {
         mp.reset();
         println!("resetting movement points.");
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum CheckDifficulty {
+    Simple,
+    Easy,
+    Routine,
+    Average,
+    Difficult,
+    VeryDifficult,
+    Formidable,
+    Impossible,
+}
+pub enum BoonOrBane {
+    Boon,
+    Bane,
+}
+
+pub enum RollResult {
+    Success(i32),
+    Failure(i32),
+}
+pub fn task_check(
+    dm: i32,
+    check_difficulty: CheckDifficulty,
+    boon_or_bane: Option<BoonOrBane>,
+) -> RollResult {
+    let difficulty = match check_difficulty {
+        CheckDifficulty::Simple => 2,
+        CheckDifficulty::Easy => 4,
+        CheckDifficulty::Routine => 6,
+        CheckDifficulty::Average => 8,
+        CheckDifficulty::Difficult => 10,
+        CheckDifficulty::VeryDifficult => 12,
+        CheckDifficulty::Formidable => 14,
+        CheckDifficulty::Impossible => 16,
+    };
+    if boon_or_bane.is_some() {
+        let effect = (roll_3d6(boon_or_bane.unwrap()) + dm) - difficulty;
+        if effect >= 0 {
+            RollResult::Success(effect)
+        } else {
+            RollResult::Failure(effect)
+        }
+    } else {
+        let effect = (roll_2d6() + dm) - difficulty;
+        if effect >= 0 {
+            RollResult::Success(effect)
+        } else {
+            RollResult::Failure(effect)
+        }
+    }
+}
+
+///randomly generates the result of rolling 2d6s
+pub fn roll_2d6() -> i32 {
+    rand::srand(rand::rand() as u64);
+    rand::gen_range(1, 6) + rand::gen_range(1, 6)
+}
+pub fn roll_3d6(boon_or_bane: BoonOrBane) -> i32 {
+    let raw_roll: Vec<i32> = vec![
+        rand::gen_range(1, 6),
+        rand::gen_range(1, 6),
+        rand::gen_range(1, 6),
+    ];
+    match boon_or_bane {
+        BoonOrBane::Bane => raw_roll.iter().sum::<i32>() - raw_roll.iter().max().unwrap(),
+        BoonOrBane::Boon => raw_roll.iter().sum::<i32>() - raw_roll.iter().min().unwrap(),
     }
 }
